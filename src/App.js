788db3000f52a8f819536cd7a34fce2bb3e71c58
Heudiini <77001762+Heudiini@users.noreply.github.com>
2022-02-01 23:14:01 +0200

    took off default values + css

--- src/App.js
+++ src/App.js
@@ -1,73 +1,42 @@
 //Tutorial from:
-//www.youtube.com/watch?v=w7ejDZ8SWv8// 
+//www.youtube.com/watch?v=w7ejDZ8SWv8//
 ///
 
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
-import { useState} from "react"
-import Header from "./components/Header"
-import Tasks from "./components/Tasks"
-import AddTask from "./components/AddTask"
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  // add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
-const App = ()  =>{ 
-  const [showAddTask, setShowAddTask] = useState
-  (false)
-  const[tasks,setTasks]= useState([
-    { 
-        id:1,
-        text: "meeting",
-        day: "December 5:th  at 20:00" ,
-        reminder:true,
-    },
-    {
-        id:2,
-        text:"shop",
-        day:"January 1.st at 9:00",
-        reminder: false,
-    }
-    
-     ])
-// add task
-const addTask = (task) => {
-  const id  = Math.floor(Math.random() * 
-  10000) + 1
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask])
-}
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-
-
-
-
-
-//delete task
-const deleteTask = (id)=> {
-  setTasks(tasks.filter((task) => task.id !== id))
-}
- 
-// toggle reminder
-const toggleReminder = (id) => {
-
-setTasks(tasks.map((task)=> task.id === id ? { ...task, reminder:
-   !task.reminder}: task
-   )
-   )
-}
-
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)));
+  };
 
   return (
     <div className="container">
-     <Header onAdd = {() => setShowAddTask
-      (!showAddTask)}
-       showAdd = {showAddTask}
-      /> 
-     {showAddTask && <AddTask onAdd = {addTask}/>}
-     {tasks.length > 0 ? (
-     <Tasks tasks={tasks} onDelete=
-     {deleteTask} onToggle={toggleReminder}/>
-      )
-       :( 
-         "There´s nothing"
-       )}
- </div>
-  )}
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "There´s nothing"
+      )}
+    </div>
+  );
+};
 export default App;
